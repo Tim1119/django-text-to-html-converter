5bd1af5c20ee3951973f9fe510137188eeb0dca0
tim1119 <timothyinioluwa@gmail.com>
2022-08-10 01:03:32 +0100

    first commit

---
+++ texteditor/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from .models import Editor
+from .forms import EditorForm
+def index(request):
+    form=EditorForm()
+    return render(request,'index.html',{'form':form})
